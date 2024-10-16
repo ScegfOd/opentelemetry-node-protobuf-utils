0c0a02541ea808a1ddace4f2f6d367cc81a94165
Jonathan Combs <JOfCombs@gmail.com>
2024-10-16 18:58:07 -0400

    add base_span

---
+++ base_span.js
@@ -0,0 +1,26 @@
+const api = require('@opentelemetry/api');
+
+module.exports = ( // same return as f(span) given as the third argument
+  tracer,   // tracer: api.tracer (api from @opentelemetry/api)
+  span_name,// span_name: string (unique name for this span)
+  f,        // f is a function that takes span as its only argument
+) => {
+  return tracer.startActiveSpan(span_name
+    , {kind: 1} // this is running on a server
+    , span=>{
+      try {
+        const result = f(span);
+        if(span.status.code == api.SpanStatusCode.UNSET)
+          span.setStatus({code: api.SpanStatusCode.OK});
+        return result;
+      } catch (err) {
+        span.setStatus({
+          code: api.SpanStatusCode.ERROR,
+          message: err.message,
+        });
+      } finally {
+        span.end();
+      }
+    }
+  );
+};
